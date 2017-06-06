265da56e3177b86b0aecfd4dabdb8f799d9f8772
peterkarrer <peter@karrer.pro>
2017-06-06 17:25:17 +0200

    added type definition for textarea control

---
+++ lib/TextAreaInput.d.ts
@@ -0,0 +1,23 @@
+/// <reference types="react" />
+import * as React from "react";
+export interface TextAreaInputProps extends React.HTMLProps<HTMLTextAreaElement> {
+    className?: string;
+    label: string;
+    inputRef?: (input: HTMLTextAreaElement) => void;
+}
+export interface TextAreaInputState {
+    empty: boolean;
+    validationMessage: string;
+}
+export declare class TextAreaInput extends React.PureComponent<TextAreaInputProps, TextAreaInputState> {
+    constructor(props?: TextAreaInputProps);
+    private id;
+    private ref;
+    private handleChange;
+    private handleRef;
+    private updateHeight();
+    private updateState();
+    componentWillMount(): void;
+    componentDidUpdate(): void;
+    render(): JSX.Element;
+}
