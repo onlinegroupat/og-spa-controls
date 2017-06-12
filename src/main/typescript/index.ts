1ae8214fa7965a924d4547ab2137a0bd4ee11893
peterkarrer <peter@karrer.pro>
2017-06-12 17:27:32 +0200

    added export for checkbox group to index

--- src/main/typescript/index.ts
+++ src/main/typescript/index.ts
@@ -1,7 +1,7 @@
 export { TextInput, TextInputProps } from "./TextInput";
 export { SelectField, SelectFieldProps } from "./SelectField";
 export { TextAreaInput, TextAreaInputProps } from "./TextAreaInput";
-export { Checkbox, CheckboxProps } from "./Checkbox";
+export { Checkbox, CheckboxProps, CheckboxGroup, CheckboxGroupProps } from "./Checkbox";
 export { Button, ButtonProps } from "./Button";
 export { Icon } from "./Icon";
 export { MaterialIcon } from "./MaterialIcon";
